b6a92c045f63a06afaea11ac58e79766423db37d
Uno Wu <st9007a@gmail.com>
2026-08-12 21:15:39 -0700

    Fix compilation error on grouped mm v3

--- src/ml_kernel_lab/kernel/triton_kernel/moe.py
+++ src/ml_kernel_lab/kernel/triton_kernel/moe.py
@@ -159,7 +159,6 @@ def moe_grouped_expert_gemm_fwd_v3_fused_kernel(
     N: tl.constexpr,
     K: tl.constexpr,
     N_EXPERTS: tl.constexpr,
-    N_EXPERTS_PAD: tl.constexpr,
     GROUP_SIZE_M: tl.constexpr,
     BLOCK_M: tl.constexpr,
     BLOCK_N: tl.constexpr,
@@ -172,63 +171,55 @@ def moe_grouped_expert_gemm_fwd_v3_fused_kernel(
     total_tiles = 0
 
     num_n_tiles = (N + BLOCK_N - 1) // BLOCK_N
+    expert_tile_start = 0
     num_tiles_in_group = GROUP_SIZE_M * num_n_tiles
 
-    expert_offsets_cols = tl.arange(0, N_EXPERTS_PAD)
-    expert_offsets = tl.load(expert_offsets_ptr + expert_offsets_cols, mask=expert_offsets_cols < N_EXPERTS + 1, other=0)
-    expert_id = 0
-    m_tiles_acc = tl.zeros((N_EXPERTS_PAD,), dtype=tl.int32)
+    for expert_id in tl.range(N_EXPERTS):
+        expert_start = tl.load(expert_offsets_ptr + expert_id)
+        expert_end = tl.load(expert_offsets_ptr + expert_id + 1)
 
-    for i in tl.range(1, N_EXPERTS + 1):
-        expert_m = expert_offsets[i] - expert_offsets[i - 1]
-        num_m_tiles = tl.cdiv(expert_m, BLOCK_M)
-        total_tiles += num_m_tiles * num_n_tiles
-        m_tiles_acc[i] = m_tiles_acc[i - 1] + num_m_tiles
-        expert_id = tl.where(tile_id >= total_tiles, i, expert_id)
+        num_m_tiles = tl.cdiv(expert_end - expert_start, BLOCK_M)
+        expert_tile_end = expert_tile_start + num_m_tiles * num_n_tiles
 
-    while tile_id < total_tiles:
-        while tile_id >= m_tiles_acc[expert_id + 1] * num_n_tiles:
-            expert_id += 1
+        while tile_id >= expert_tile_start and tile_id < expert_tile_end:
+            tile_offset = tile_id - expert_tile_start
+            group_id = tile_offset // num_tiles_in_group
+            first_m = group_id * GROUP_SIZE_M
+            group_size_m = min(num_m_tiles - first_m, GROUP_SIZE_M)
 
-        expert_tile_start = m_tiles_acc[expert_id] * num_n_tiles
-        tile_offset = tile_id - expert_tile_start
-        group_id = tile_offset // num_tiles_in_group
-        first_m = group_id * GROUP_SIZE_M
-        num_m_tiles = m_tiles_acc[expert_id + 1] - m_tiles_acc[expert_id]
-        group_size_m = min(num_m_tiles - first_m, GROUP_SIZE_M)
+            tile_in_group = tile_offset % num_tiles_in_group
+            m_tile_id = first_m + tile_in_group % group_size_m
+            n_tile_id = tile_in_group // group_size_m
 
-        tile_in_group = tile_offset % num_tiles_in_group
-        m_tile_id = first_m + tile_in_group % group_size_m
-        n_tile_id = tile_in_group // group_size_m
+            m_start = expert_start + m_tile_id * BLOCK_M
+            n_start = n_tile_id * BLOCK_N
 
-        m_start = expert_offsets[expert_id] + m_tile_id * BLOCK_M
-        n_start = n_tile_id * BLOCK_N
+            rows = m_start + tl.arange(0, BLOCK_M)
+            cols = n_start + tl.arange(0, BLOCK_N)
 
-        rows = m_start + tl.arange(0, BLOCK_M)
-        cols = n_start + tl.arange(0, BLOCK_N)
+            k_offsets = tl.arange(0, BLOCK_K)
+            m_mask = rows < expert_end
+            n_mask = cols < N
 
-        k_offsets = tl.arange(0, BLOCK_K)
-        m_mask = rows < expert_offsets[expert_id + 1]
-        n_mask = cols < N
+            acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.float32)
 
-        acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.float32)
+            for k_start in tl.range(0, K, BLOCK_K):
+                k_start_offsets = k_start + k_offsets
+                k_mask = k_start_offsets < K
 
-        for k_start in tl.range(0, K, BLOCK_K):
-            k_start_offsets = k_start + k_offsets
-            k_mask = k_start_offsets < K
+                m_tile_offsets = rows[:, None] * x_stride_row + k_start_offsets[None, :]
+                m_tile = tl.load(x_grouped_ptr + m_tile_offsets, mask=m_mask[:, None] & k_mask[None, :], other=0.)
 
-            m_tile_offsets = rows[:, None] * x_stride_row + k_start_offsets[None, :]
-            m_tile = tl.load(x_grouped_ptr + m_tile_offsets, mask=m_mask[:, None] & k_mask[None, :], other=0.)
+                n_tile_offsets = expert_id * w_stride_e + k_start_offsets[:, None] * w_stride_row + cols[None, :]
+                n_tile = tl.load(w_ptr + n_tile_offsets, mask=k_mask[:, None] & n_mask[None, :], other=0.)
 
-            n_tile_offsets = expert_id * w_stride_e + k_start_offsets[:, None] * w_stride_row + cols[None, :]
-            n_tile = tl.load(w_ptr + n_tile_offsets, mask=k_mask[:, None] & n_mask[None, :], other=0.)
+                acc = tl.dot(m_tile, n_tile, acc=acc)
 
-            acc = tl.dot(m_tile, n_tile, acc=acc)
+            out_offsets = rows[:, None] * out_stride_row + cols[None, :]
+            tl.store(out_ptr + out_offsets, acc, mask=m_mask[:, None] & n_mask[None, :])
 
-        out_offsets = rows[:, None] * out_stride_row + cols[None, :]
-        tl.store(out_ptr + out_offsets, acc, mask=m_mask[:, None] & n_mask[None, :])
-
-        tile_id += step
+            tile_id += step
+        expert_tile_start = expert_tile_end
 
 
 moe_grouped_expert_gemm_fwd_v1_autotuned_fused_kernel = triton.autotune(
@@ -559,7 +550,6 @@ def moe_grouped_expert_gemm_fwd_v3(
         N,
         K,
         n_experts,
-        triton.next_power_of_2(n_experts + 1),
         GROUP_SIZE_M,
         BLOCK_M,
         BLOCK_N,
